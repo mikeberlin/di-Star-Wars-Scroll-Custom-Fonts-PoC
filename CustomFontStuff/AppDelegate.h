2f6a9b5af144cb99456cc3816bd0a8bc341482c7
Mike Berlin <mikeberlin@me.com>
2013-05-01 18:39:20 -0500

    Initial Commit

---
+++ CustomFontStuff/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomFontStuff
+//
+//  Created by Mike Berlin on 5/1/13.
+//  Copyright (c) 2013 Mike Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
